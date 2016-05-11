0bc8464f627cbab176c4be7ba332b74e1a1ef26f
root <root@bzielinski91-Lenovo-B570e>
2016-05-11 18:37:06 +0200

    Sortowanie zdjęć w kolekcji(liście) po nazwie pliku

--- ZdjDrogowe/src/main/java/com/mycompany/zdjdrogowe/Methods.java
+++ ZdjDrogowe/src/main/java/com/mycompany/zdjdrogowe/Methods.java
@@ -6,6 +6,9 @@
 package com.mycompany.zdjdrogowe;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  *
@@ -14,26 +17,29 @@ import java.io.File;
 public class Methods {
 
     public void search() {
-        File file = new File("gotowaBaza");
+        File file = new File("../../gotowaBaza");
         listFilesForFolder(file);
     }
 
     public void listFilesForFolder(final File folder) {
+        List<File> pictures = new ArrayList();
         for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                listFilesForFolder(fileEntry);
-            } else {
-                String extension = "";
+            
+            String extension = "";
 
-                int i = fileEntry.getName().lastIndexOf('.');
-                if (i > 0) {
-                    extension = fileEntry.getName().substring(i + 1);
-                }
-                if (extension.equals("ppm")) {
-                    System.out.println(fileEntry.getName());
-                }
+            int i = fileEntry.getName().lastIndexOf('.');
+            if (i > 0) {
+                extension = fileEntry.getName().substring(i + 1);
+            }
+            if (extension.equals("ppm")) {
+                pictures.add(fileEntry);
             }
         }
+        Comparator<File> c = (s1, s2) -> s1.getName().compareTo(s2.getName());
+        pictures.sort(c);
+        pictures.stream().forEach((pict) -> {
+            System.out.println(pict.getName());
+        });
     }
 
 }
