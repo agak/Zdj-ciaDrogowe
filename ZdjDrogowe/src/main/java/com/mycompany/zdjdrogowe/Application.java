f4b273c7e42aa19d3e16ea926a7a6c01414f3fb6
agnieszka <agnieszka@agnieszka-HP-Pavilion-Gaming-Notebook>
2016-05-14 22:27:22 +0200

    poprawienie błędu(wychodzenie poza zakres) i zapis wyniku metody do pliku

--- ZdjDrogowe/src/main/java/com/mycompany/zdjdrogowe/Application.java
+++ ZdjDrogowe/src/main/java/com/mycompany/zdjdrogowe/Application.java
@@ -14,17 +14,23 @@ import java.util.List;
  * @author agnieszka
  */
 public class Application {
-    
-    
-    public static void main(String [] args){
+
+    private static double[][] blue = {{120.0, 255.0}, {150.0, 255.0}, {120.0, 250.0}}; //blue
+    private static double[][] red = {{142.0, 255.0}, {27.0, 86.0}, {28.0, 87.0}}; //czerwony
+    private static double[][] blue2 = {{32.0, 61.0}, {43.0, 77.0}, {96.0, 121.0}}; //niebieski
+    private static double[][] white = {{170.0, 255.0}, {170.0, 255.0}, {201.0, 255.0}}; //biały
+
+    public static void main(String[] args) {
         File file = new File("../../gotowaBaza");
         Methods methods = new Methods();
-        
+
         List<File> pictures = new ArrayList();
         pictures = methods.makePicturesList(file);
-        
-        methods.loadImages(pictures);
-        
+        System.out.println("Kolor niebieski: ");
+        methods.loadImagesAndSaveToFile(pictures, blue2);
+//        System.out.println("Kolor czerwony: ");
+//        methods.loadImagesAndSaveToFile(pictures, red);
+
     }
-    
+
 }
--- ZdjDrogowe/src/main/java/com/mycompany/zdjdrogowe/Methods.java
+++ ZdjDrogowe/src/main/java/com/mycompany/zdjdrogowe/Methods.java
@@ -7,6 +7,7 @@ package com.mycompany.zdjdrogowe;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,17 +20,15 @@ import javax.imageio.ImageIO;
  * @author agnieszka
  */
 public class Methods {
-    
-    private double[][] color = {{120.0, 255.0}, {150.0, 255.0}, {120.0, 250.0}}; //blue
-    
 
-    
+    FileOutputStream fos = null;
+
     public List<File> makePicturesList(File folder) {
         List<File> pictures = new ArrayList();
         for (final File fileEntry : folder.listFiles()) {
-            
+
             String extension = "";
-            
+
             int i = fileEntry.getName().lastIndexOf('.');
             if (i > 0) {
                 extension = fileEntry.getName().substring(i + 1);
@@ -40,94 +39,123 @@ public class Methods {
         }
         Comparator<File> c = (s1, s2) -> s1.getName().compareTo(s2.getName());
         pictures.sort(c);
-        return pictures;  
+        return pictures;
     }
-    
-    public void loadImages(List<File> pictures){
+
+    public void loadImagesAndSaveToFile(List<File> pictures, double[][] color) {
+        try {
+            fos = new FileOutputStream("plik.txt");
+        } catch (IOException ex) {
+            System.out.println("Błąd operacji na pliku: " + ex);
+        }
+
         pictures.stream().forEach((pict) -> {
             System.out.println(pict.getName());
-            loadImage(pict);
+            loadImage(pict, color);
         });
+
+        try {
+            fos.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    private void loadImage(File fileEntry) {
+
+    private void loadImage(File fileEntry, double[][] color) {
         BufferedImage imageToChoice = null;
         try {
             imageToChoice = ImageIO.read(fileEntry);
         } catch (IOException e) {
             System.err.println("Blad odczytu obrazka");
         }
-        
-        int[] cordinatePixel=findPixelWithColor(imageToChoice, color);
+
+        int[] cordinatePixel = findPixelWithColor(imageToChoice, color);
         //segmentacja
         List<Integer> XCoordinatesOfArea = new ArrayList();
         List<Integer> YCoordinatesOfArea = new ArrayList();
-        designateArea(imageToChoice, cordinatePixel,XCoordinatesOfArea,YCoordinatesOfArea);
-        
+        designateArea(imageToChoice, cordinatePixel, XCoordinatesOfArea, YCoordinatesOfArea, color);
+
         //oznaczenie wyznaczonej przestrzeni (znaku drogowego) w prostokąt
-        markAreaInARectangle(XCoordinatesOfArea, YCoordinatesOfArea);
+        int[] rectangleCoordinate = markAreaInARectangle(XCoordinatesOfArea, YCoordinatesOfArea);
+        saveRowToFile(fileEntry, rectangleCoordinate);
     }
-    
+
     private int[] findPixelWithColor(BufferedImage imageToChoice, double[][] color) {
         double[] pixel = new double[3];
-        int [] cordinate= new int [2];
+        int[] cordinate = new int[2];
         outerloop:
         for (int a = 0; a < imageToChoice.getWidth(); a++) {
             for (int b = 0; b < imageToChoice.getHeight(); b++) {
                 imageToChoice.getRaster().getPixel(a, b, pixel);
-                
+
                 if (pixel[0] > color[0][0] && pixel[0] < color[0][1] && pixel[1] > color[1][0] && pixel[1] < color[1][1] & pixel[2] > color[2][0] && pixel[2] < color[2][1]) {
-                   cordinate[0]=a;
-                    cordinate[1]=b;
+                    cordinate[0] = a;
+                    cordinate[1] = b;
                     break outerloop;
                 }
                 //System.out.println(a+" "+b+" "+pixel[0]+" "+pixel[1]+" "+pixel[2]+" ");
             }
         }
-        System.out.println("PixelWithColor: "+cordinate[0]+" "+cordinate[1]);
+        System.out.println("PixelWithColor: " + cordinate[0] + " " + cordinate[1]);
         return cordinate;
     }
-    
-    
-    private void designateArea(BufferedImage imageToChoice, int[] cordinatePixel,List<Integer> XCoordinatesOfArea,List<Integer> YCoordinatesOfArea){
-        
+
+    private void designateArea(BufferedImage imageToChoice, int[] cordinatePixel, List<Integer> XCoordinatesOfArea, List<Integer> YCoordinatesOfArea, double[][] color) {
+
         double[] colorRGB = new double[3];
 
-        for(int x=cordinatePixel[0]-1;x<=cordinatePixel[0]+1;x++){
-            for(int y=cordinatePixel[1]-1;y<=cordinatePixel[1]+1;y++){
-                imageToChoice.getRaster().getPixel(x, y, colorRGB);
-                if(colorRGB[0] > color[0][0] && colorRGB[0] < color[0][1] && colorRGB[1] > color[1][0] && colorRGB[1] < color[1][1] & colorRGB[2] > color[2][0] && colorRGB[2] < color[2][1]){
-                    if((!XCoordinatesOfArea.contains(x)) || (!YCoordinatesOfArea.contains(y))){
-                        if(!XCoordinatesOfArea.contains(x)){
-                            XCoordinatesOfArea.add(x);
+        for (int x = cordinatePixel[0] - 1; x <= cordinatePixel[0] + 1; x++) {
+            for (int y = cordinatePixel[1] - 1; y <= cordinatePixel[1] + 1; y++) {
+                if (x > 0 && x < imageToChoice.getWidth() && y > 0 && y < imageToChoice.getHeight()) {
+                    imageToChoice.getRaster().getPixel(x, y, colorRGB);
+                    if (colorRGB[0] > color[0][0] && colorRGB[0] < color[0][1] && colorRGB[1] > color[1][0] && colorRGB[1] < color[1][1] & colorRGB[2] > color[2][0] && colorRGB[2] < color[2][1]) {
+                        if ((!XCoordinatesOfArea.contains(x)) || (!YCoordinatesOfArea.contains(y))) {
+                            if (!XCoordinatesOfArea.contains(x)) {
+                                XCoordinatesOfArea.add(x);
+                            }
+                            if (!YCoordinatesOfArea.contains(y)) {
+                                YCoordinatesOfArea.add(y);
+                            }
+
+                            cordinatePixel[0] = x;
+                            cordinatePixel[1] = y;
+                            designateArea(imageToChoice, cordinatePixel, XCoordinatesOfArea, YCoordinatesOfArea, color);
                         }
-                        if(!YCoordinatesOfArea.contains(y)){
-                            YCoordinatesOfArea.add(y);
-                        }
-                        
-                        cordinatePixel[0]=x;
-                        cordinatePixel[1]=y;
-                        designateArea(imageToChoice, cordinatePixel, XCoordinatesOfArea, YCoordinatesOfArea);
                     }
                 }
             }
         }
     }
-    
-    private void markAreaInARectangle(List XCoordinatesOfArea, List YCoordinatesOfArea){
-        int Xmin=(int) Collections.min(XCoordinatesOfArea);
-        int Xmax=(int) Collections.max(XCoordinatesOfArea);
-        int Ymin=(int) Collections.min(YCoordinatesOfArea);
-        int Ymax=(int) Collections.max(YCoordinatesOfArea);
+
+    private int[] markAreaInARectangle(List XCoordinatesOfArea, List YCoordinatesOfArea) {
+        int[] rectangleCoordinate = {-1, -1, -1, -1};  //-1 jeśli nie znaleziono znaku
+        if (XCoordinatesOfArea.size() > 0) {
+            rectangleCoordinate[0] = (int) Collections.min(XCoordinatesOfArea);
+            rectangleCoordinate[1] = (int) Collections.max(XCoordinatesOfArea);
+            rectangleCoordinate[2] = (int) Collections.min(YCoordinatesOfArea);
+            rectangleCoordinate[3] = (int) Collections.max(YCoordinatesOfArea);
+        }
         /*XCoordinatesOfArea.stream().forEach((Xc) -> {
             System.out.println("Xcord: "+Xc);
         });
         YCoordinatesOfArea.stream().forEach((Yc) -> {
             System.out.println("Ycord: "+Yc);
         });*/
-        System.out.println("Xmin: "+Xmin+"; Xmax: "+Xmax+"; Ymin: "+Ymin+"; Ymax: "+Ymax);
-        
-        //do napisania zapisywanie do pliku tekstowego wyników
+        System.out.println("Xmin: " + rectangleCoordinate[0] + "; Xmax: " + rectangleCoordinate[1] + "; Ymin: " + rectangleCoordinate[2] + "; Ymax: " + rectangleCoordinate[3]);
+
+        return rectangleCoordinate;
     }
-    
+
+    private void saveRowToFile(File imageFile, int[] rectangleCoordinate) {
+        String output = imageFile.getName() + ";" + rectangleCoordinate[0] + ";" + rectangleCoordinate[1] + ";" + rectangleCoordinate[2] + ";" + rectangleCoordinate[3] + "\n";
+
+        try {
+            for (int i = 0; i < output.length(); i++) {
+                fos.write((int) output.charAt(i));
+            }
+        } catch (IOException ex) {
+            System.out.println("Błąd operacji na pliku: " + ex);
+        }
+
+    }
 }
